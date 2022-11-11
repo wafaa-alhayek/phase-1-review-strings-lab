4a77fe1a31c43b385766811472469ab3d08f3cc8
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2022-11-11 23:56:12 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const firstInitial = currentUser[0];
+ const restOfName = currentUser.slice(1)
+const shortGreeting = `Welcome, ${firstInitial}!`
